922ded82213089c5b561a80def108df6de87fbca
raulf <rfkao@iscte-iul.pt>
2025-12-06 00:35:42 +0000

    adicao do MainPageTest by Raul como MainPageTest1

---
+++ src/test/java/com/example/demo/MainPageTest1.java
@@ -0,0 +1,78 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class MainPageTest1 {
+    private WebDriver driver;
+    private MainPage mainPage;
+
+
+    @BeforeEach
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.jetbrains.com/");
+
+        // Fecha o pop-up de cookies se existir
+        try {
+            WebElement acceptCookies = driver.findElement(
+                    By.cssSelector("button[class='ch2-btn ch2-allow-all-btn ch2-btn-primary']")
+            );
+            acceptCookies.click();
+        } catch (Exception ignored) {
+            // Se não aparecer, continua normalmente
+        }
+
+        mainPage = new MainPage(driver);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void search() {
+        mainPage.searchButton.click();
+
+        WebElement searchField = driver.findElement(By.cssSelector("input[data-test$='inner']"));
+        searchField.sendKeys("Selenium");
+
+        WebElement submitButton = driver.findElement(By.cssSelector("button[data-test='full-search-button']"));
+        submitButton.click();
+
+        WebElement searchPageField = driver.findElement(By.cssSelector("input[data-test$='inner']"));
+        assertEquals("Selenium", searchPageField.getAttribute("value"));
+    }
+
+    @Test
+    public void toolsMenu() {
+        mainPage.toolsMenu.click();
+
+        WebElement menuPopup = driver.findElement(By.cssSelector("div[class*='71'] button[data-test='main-menu-item-action']"));
+        assertTrue(menuPopup.isDisplayed());
+    }
+
+    @Test
+    public void navigationToAllTools() {
+        mainPage.seeDeveloperToolsButton.click();
+        //Thread.sleep(2000);
+        mainPage.findYourToolsButton.click();
+        //Thread.sleep(2000);
+
+
+        WebElement productsList = driver.findElement(By.id("products-page"));
+        assertTrue(productsList.isDisplayed());
+        assertEquals("All Developer Tools and Products by JetBrains", driver.getTitle());
+    }
+}
